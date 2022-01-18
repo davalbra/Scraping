1cca51241f74739dc960936f1774981fceeb4b41
GBaquer25 <57840050+GBaquer25@users.noreply.github.com>
2022-01-18 13:31:28 -0500

    Scrapear una tabla
    Aun falta pulir el método

--- main.rb
+++ main.rb
@@ -1,11 +1,18 @@
-puts "Scraper StackOverflow"
-require 'open-uri'
-require 'nokogiri'
-require 'csv'
-require './Extractor.rb'
+link = "https://aulapro.co/especiales-aulapro/los-100-mejores-cursos-de-coursera-de-todos-los-tiempos/"
 
-link = "https://www.jetbrains.com/es-es/lp/devecosystem-2021/"
-
-p1 = Extractor.new(link)
-puts p1
-p1.obtener()
+ def self.data_from_link(link)
+        doc = Nokogiri::HTML(open(link))
+        @occurances = []
+        doc.xpath("//tr").each do |x|
+          posicion = doc.x("td")[0].text
+          nombre_curso = doc.x("td")[1].text
+          calificacion = doc.x("td")[2].text
+          valoraciones = doc.x("td")[4].text
+          categoria = doc.x("td")[5].text
+          source = doc.search(".links li.last a").attr("href").value
+          @occurances << {:posicion => posicion, :nombre_curso => nombre_curso, :calificacion => calificacion, :valoraciones => valoraciones, :categoria => categoria, :source => source}
+        end
+        puts @occurances
+      end
+  
+puts "Fin de práctica"
