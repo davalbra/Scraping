df4b8723380a58a18fa5b66cfbaa1135c03cd355
ycedenog <yonmaced@espol.edu.ec>
2022-02-01 21:36:55 -0500

    scrap

---
+++ Yonkani/main.rb
@@ -0,0 +1,20 @@
+require 'nokogiri'
+
+puts "Scraper StackOverflow"
+require 'open-uri'
+require 'nokogiri'
+require 'csv'
+require './Extractor.rb'
+require './extractorContribution.rb'
+
+linkGithub = "https://octoverse.github.com/"
+linkPush1 = "https://madnight.github.io/githut/#/pushes/2021/1"
+linkPush4 = "https://madnight.github.io/githut/#/pushes/2021/4"
+
+
+
+p1 = Extractor.new(linkGithub)
+p2 = ExtractorContribution.new(linkPush1)
+
+p2.obtenerContribucion()
+
